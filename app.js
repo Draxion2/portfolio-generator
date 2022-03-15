79be47560595728d61e1577dbcf1e2a5a929d1cd
Holdan McMullen <holdanmcmullen@gmail.com>
2022-03-15 02:36:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+  name: 'Lernantino',
+  age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+
+const printProfileData = (profileDataArr) => {
+  console.log(profileDataArr);
+};
+
+printProfileData(profileDataArgs);
